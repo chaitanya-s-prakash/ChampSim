0fcb3dd33b1a4f5ac1fc5904d63f92ff55dfaf3e
Chaitanya S Prakash <cprakash2@wisc.edu>
2026-05-03 23:53:20 -0500

    berti: add core helpers and constants

--- prefetcher/berti/berti.cc
+++ prefetcher/berti/berti.cc
@@ -2,6 +2,44 @@
 
 #include <fmt/core.h>
 
+#include "cache.h"
+
+uint64_t berti::current_cycle() const
+{
+  if (intern_->clock_period.count() == 0)
+    return 0;
+
+  return static_cast<uint64_t>(intern_->current_time.time_since_epoch() / intern_->clock_period);
+}
+
+uint64_t berti::line_number(champsim::address addr)
+{
+  return addr.to<uint64_t>() >> LOG2_BLOCK_SIZE;
+}
+
+champsim::address berti::address_from_line(uint64_t line)
+{
+  return champsim::address{line << LOG2_BLOCK_SIZE};
+}
+
+uint64_t berti::ip_hash(champsim::address ip)
+{
+  const auto raw_ip = ip.to<uint64_t>();
+  return raw_ip ^ (raw_ip >> 16) ^ (raw_ip >> 32);
+}
+
+uint64_t berti::history_ip_tag(champsim::address ip)
+{
+  const auto mask = (uint64_t{1} << HISTORY_IP_TAG_BITS) - 1;
+  return (ip.to<uint64_t>() >> 3) & mask;
+}
+
+uint64_t berti::delta_table_ip_tag(champsim::address ip)
+{
+  const auto mask = (uint64_t{1} << DELTA_TABLE_IP_TAG_BITS) - 1;
+  return ip_hash(ip) & mask;
+}
+
 void berti::prefetcher_initialize()
 {
   fmt::print("Berti prefetcher initialized\n");
--- prefetcher/berti/berti.h
+++ prefetcher/berti/berti.h
@@ -1,6 +1,7 @@
 #ifndef PREFETCHER_BERTI_H
 #define PREFETCHER_BERTI_H
 
+#include <cstddef>
 #include <cstdint>
 
 #include "access_type.h"
@@ -8,6 +9,44 @@
 #include "modules.h"
 
 class berti : public champsim::modules::prefetcher {
+  enum class delta_status : uint8_t {
+    none,
+    l2_pref,
+    l2_pref_repl,
+    l1_pref,
+  };
+
+  static constexpr std::size_t HISTORY_SETS = 8;
+  static constexpr std::size_t HISTORY_WAYS = 16;
+  static constexpr std::size_t HISTORY_IP_TAG_BITS = 7;
+  static constexpr std::size_t HISTORY_LINE_BITS = 24;
+  static constexpr std::size_t HISTORY_TIMESTAMP_BITS = 16;
+
+  static constexpr std::size_t DELTA_TABLE_ENTRIES = 16;
+  static constexpr std::size_t DELTAS_PER_ENTRY = 16;
+  static constexpr std::size_t DELTA_TABLE_IP_TAG_BITS = 10;
+  static constexpr std::size_t DELTA_BITS = 13;
+  static constexpr std::size_t COVERAGE_COUNTER_BITS = 4;
+
+  static constexpr std::size_t LEARNING_ROUNDS = 16;
+  static constexpr std::size_t MAX_TIMELY_DELTAS = 8;
+  static constexpr std::size_t MAX_SELECTED_DELTAS = 12;
+
+  static constexpr uint64_t L1_COVERAGE_THRESHOLD = 10;
+  static constexpr uint64_t L2_COVERAGE_THRESHOLD = 5;
+  static constexpr uint64_t L2_REPLACEABLE_COVERAGE_THRESHOLD = 8;
+  static constexpr double L1_MSHR_OCCUPANCY_THRESHOLD = 0.70;
+
+  static constexpr uint64_t WARMUP_MIN_SEARCHES = 8;
+  static constexpr uint64_t WARMUP_COVERAGE_PERCENT = 80;
+
+  [[nodiscard]] uint64_t current_cycle() const;
+  [[nodiscard]] static uint64_t line_number(champsim::address addr);
+  [[nodiscard]] static champsim::address address_from_line(uint64_t line);
+  [[nodiscard]] static uint64_t ip_hash(champsim::address ip);
+  [[nodiscard]] static uint64_t history_ip_tag(champsim::address ip);
+  [[nodiscard]] static uint64_t delta_table_ip_tag(champsim::address ip);
+
 public:
   using prefetcher::prefetcher;
 
